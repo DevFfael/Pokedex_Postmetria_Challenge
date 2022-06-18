a1846784403b57d91dcffbe46baf962dc4a17444
Raffael Oliveira <79339925+RaffaelOliveira-dev@users.noreply.github.com>
2022-06-18 01:00:04 -0300

    Style list

--- src/components/PokeCard/index.tsx
+++ src/components/PokeCard/index.tsx
@@ -1,36 +1,50 @@
-import { Flex, Image, Text, useColorModeValue } from "@chakra-ui/react";
-import React from "react";
+import { Flex, Image, Text} from "@chakra-ui/react";
+import { transform } from "framer-motion";
+import React, { useEffect, useState } from "react";
+import api from '../../pages/api'
 
-export function PokeTable(){
-  let boxBg = useColorModeValue("white !important", "#111c44 !important");
-  let mainText = useColorModeValue("gray.800", "white");
-  let secondaryText = useColorModeValue("gray.400", "gray.400");
+export function PokeCard({pokemon}){
+
+  const [pokemonsInfo, setPokemonsInfo] = useState({})
+
+  const fetchPokemonInfo = async () => {
+    const result = await api.get(`${pokemon.url}`)
+    console.log(result.data)
+    setPokemonsInfo(result.data)
+  }
+
+  useEffect(() => {fetchPokemonInfo()}, [])
 
   return (
     <Flex
       borderRadius='20px'
       bg='white'
       p='20px'
-      w='25%'
+      w='15%'
       mr='10px'
       ml='10px'
+      minW='250px'
       alignItems='center'
-      direction='column'>
+      direction='column'
+      border='1px solid #e0e0e0'
+      transition='transform 200ms ease-in-out, box-shadow 200ms ease-in-out'
+      _hover={{ boxShadow:'2xl', transform:'translateY(-5px)' }}
+      >
 
       <Image src='https://i.ibb.co/xmP2pS6/Profile.png' maxW='100%' borderRadius='10px' />
 
       <Flex flexDirection='column' mb='10px'>
         <Image
-          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png'
+          src={pokemonsInfo.sprites.front_default}
           mx='auto'
-          width='60%'
+          width='100%'
           mt='-58px'
         />
         <Text
           fontWeight='600'
           textAlign='center'
           fontSize='xl'>
-          Ditto
+          {pokemonsInfo.name}
         </Text>
       </Flex>
       <Flex justify='space-between' w='100%' px='16px'>
@@ -40,7 +54,7 @@ export function PokeTable(){
             fontSize='xl'
             color='black'
             textAlign='center'>
-            17
+            {pokemonsInfo.stats[0].base_stat}
           </Text>
           <Text color='#2f91e0' fontWeight='500'>
             HP
@@ -53,7 +67,7 @@ export function PokeTable(){
             fontSize='xl'
             textAlign='center'
             >
-            9.7k
+            {pokemonsInfo.stats[1].base_stat}
           </Text>
           <Text color='#db0909' fontWeight='500'>
             ATK
@@ -65,7 +79,7 @@ export function PokeTable(){
             fontSize='xl'
             color='black'
             textAlign='center'>
-            274
+            {pokemonsInfo.stats[2].base_stat}
           </Text>
           <Text color='#22c9be' fontWeight='500'>
             DEF
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,9 @@
 import Head from 'next/head'
-import { useEffect, useState } from 'react'
-import api from './api'
-import {PokeTable} from '../components/PokeTable/index'
-import { Flex } from '@chakra-ui/react'
+import {PokeList} from '../components/PokeList/index'
+import {Header} from '../components/Header/index'
+import { Flex, Grid } from '@chakra-ui/react'
 
 const Home = () => {
-
-  const [pokemons, setPokemons] = useState([])
-
-  const fetchPokemon = async () => {
-    const result = await api.get('pokemon/')
-    console.log(result)
-    setPokemons(result.data.results)
-  }
-
-  useEffect(() => {fetchPokemon()}, [])
-
   return (
     <div>
       <Head>
@@ -24,10 +12,11 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Flex bg='darkgray' justifyContent='center'>
-        <PokeTable />
-        <PokeTable />
-      </Flex>
+      <Header />
+
+        <Flex wrap='wrap' gap='10px' justifyContent='center' mx='300px'>
+          <PokeList />
+        </Flex>
       
     </div>
   )
