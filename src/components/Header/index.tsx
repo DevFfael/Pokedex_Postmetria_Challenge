27292d69f98b5d031158f204dfa7b2c2c50c5957
Raffael Oliveira <79339925+RaffaelOliveira-dev@users.noreply.github.com>
2022-06-19 00:36:53 -0300

    Created Project Logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Button, Flex, Image } from "@chakra-ui/react";
+import ImgLogo from '../../../public/Logo.png'
 import React from "react";
 
 export function Header(){
+
+  function RedefinirLista(){
+    localStorage.setItem("removedPokemons", "[]")
+  }
+
   return(
-    <Flex px='60px' h='3rem' my='40px' bg='tomato' justifyContent='center' alignItems='center'>
-      <Text>
-        Area da Logo
-      </Text>
+    <Flex px='60px' py='80px' justifyContent='space-between' alignItems='center'>
+      <Image src={ImgLogo.src} w='200px'/>
+      <Button onClick={RedefinirLista} colorScheme='teal' size='md'>Redefinir Lista</Button>
     </Flex>
   )
 }
